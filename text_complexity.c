aac219e66d16e8724cdddbe48be3e6894ee48911
unknown <pacholek.am@gmail.com>
2019-11-24 15:15:46 +0100

    Postepy w pracy

--- text_complexity.c
+++ text_complexity.c
@@ -5,15 +5,82 @@
 #include <string.h>
 
 
+char* read_file()
+{
+  char filename[100];
+  printf("Wczytaj plik z tekstem:\n");
+  scanf("%s", filename);
+  char *text = NULL;
+  size_t size = 0;
+  FILE *fp = fopen(filename, "r");
+  if(fp==NULL)
+  {
+   printf("Plik nie istnieje!");
+   exit(1);
+  }
+  fseek(fp, 0, SEEK_END);
+  size = ftell(fp);
+  rewind(fp);
+  text = malloc((size + 1) * sizeof(*text));
+  fread(text, size, 1, fp);
+  text[size] = '\0';
+  return text;
+}
+
+void split_text_into_sentences(char *some_text)
+{
+    printf("\nTekst podzielony na zdania:\n\n");
+    int index=0;
+    char **res=NULL;
+    char *command= malloc(strlen(some_text)+1);
+    strcpy(command, some_text);
+    char *tok = strtok(command, ".?!");
+    while(tok!=NULL) {
+        res = realloc(res, sizeof(char*)*(index+1));
+        char *dup = malloc(strlen(tok)+1);
+        strcpy(dup, tok);
+        res[index++] = dup;
+        tok = strtok(NULL, ".?!");
+    }
+
+    res = realloc(res, sizeof(char*)*(index+1));
+    res[index]=NULL;
+    free(command);
+
+    for(int j=0;j<index;j++){
+      printf("%s\n\n",res[j]);
+    }
+}
+
+void split_text_into_words(char *some_text)
+{
+    printf("\nTekst podzielony na slowa:\n\n");
+    int index=0;
+    char **res=NULL;
+    char *command= malloc(strlen(some_text)+1);
+    strcpy(command, some_text);
+    char *tok = strtok(command, " .,:!?;");
+    while(tok!=NULL) {
+        res = realloc(res, sizeof(char*)*(index+1));
+        char *dup = malloc(strlen(tok)+1);
+        strcpy(dup, tok);
+        res[index++] = dup;
+        tok = strtok(NULL, " .,:!?;");
+    }
+
+    res = realloc(res, sizeof(char*)*(index+1));
+    res[index]=NULL;
+    free(command);
+
+    for(int j=0;j<index;j++){
+      printf("%s\n",res[j]);
+    }
+}
 
 int main()
 {
    int x;
-   FILE *file;
    int text;
-   char  *words_array[1000];
-   char  *sentences_array[1000];
-
 
    printf("Witamy w programie Skladniowiec!\n");
    printf("Z jakiego trybu programu chcesz skorzystac?\n");
@@ -24,89 +91,17 @@ int main()
    switch (x) {
      case 1:
      text=read_file();
-     //printf("%s\n", text);
-     *words_array = split_text_into_words(text);
-     *sentences_array = split_text_into_sentences(text);
+     printf("\nWczytany tekst:\n");
+     printf("%s\n", text);
+     split_text_into_words(text);
+     split_text_into_sentences(text);
      break;
    case 2: break;
 }
-}
-
-int read_file(){
-char filename[100];
-printf("Wczytaj plik z tekstem:\n");
-scanf("%s", filename);
-char *text = NULL;
-size_t size = 0;
-FILE *fp = fopen(filename, "r");
-fseek(fp, 0, SEEK_END);
-size = ftell(fp);
-rewind(fp);
-text = malloc((size + 1) * sizeof(*text));
-fread(text, size, 1, fp);
-text[size] = '\0';
-return text;
+return 0;
 }
 
 
-void split_text_into_words(int some_text)
-{
-     int i =0;
-     char *words_array[1000];
-     char* words=strtok(some_text," .,:!?;");
-     while (words != NULL) {
-     words_array[i++]=words;
-     words = strtok(NULL, " .,:!?;");
-    }
-    return *words_array;
-}
-
-void split_text_into_sentences(int some_text)
-{
-     int i =0;
-     char *sentences_array[1000];
-     char* sentences=strtok(some_text,".?!");
-     while (sentences != NULL) {
-     sentences_array[i++]=sentences;
-     sentences = strtok(NULL, ".?!");
-    }
-    return *sentences_array;
-}
-
-double mean(char array[],int level)
-{
-
-}
-
-double median(char array[],int level)
-{
-
-}
-
-double sd(char array[],int level)
-{
-
-}
-
-double max(char array[],int level)
-{
-
-}
-
-double min(char array[],int level)
-{
-
-}
-
-double skewness(char array[],int level)
-{
-
-}
-
-double kurtosis(char array[],int level)
-{
-
-}
 
 
 
